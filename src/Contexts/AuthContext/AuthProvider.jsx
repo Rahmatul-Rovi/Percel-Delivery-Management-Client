74c8d8b37d7423f51eff7ddcaf8d3dd396af3e00
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-24 00:18:56 +0600

    Client Section Updated

--- src/Contexts/AuthContext/AuthProvider.jsx
+++ src/Contexts/AuthContext/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../Firebase/Firebase.init';
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
@@ -13,6 +15,11 @@ const AuthProvider = ({children}) => {
     const signIn = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
     }
 
     const logOut = () => {
@@ -35,7 +42,8 @@ const AuthProvider = ({children}) => {
         loading,
           createUser,
           signIn,
-          logOut
+          logOut,
+          signInWithGoogle
     }
     return (
         
--- src/Pages/Authentication/SocialLogin/SocilaLogin.jsx
+++ src/Pages/Authentication/SocialLogin/SocilaLogin.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import useAuth from "../../../Hooks/UseAuth";
 
 const SocilaLogin = () => {
+    const {signInWithGoogle} = useAuth();
+    const handleGoogleSignIn = () => {
+       signInWithGoogle()
+       .then(result => {
+        console.log(result.user);
+       })
+       .error(error => {
+        console.error(error);
+       })
+    }
   return (
     <div className="text-center">
       <p className="mb-4">OR</p>
-      <button className="btn bg-white text-black border-[#e5e5e5]">
+      <button onClick={handleGoogleSignIn} className="btn bg-white text-black border-[#e5e5e5]">
         <svg
           aria-label="Google logo"
           width="16"
