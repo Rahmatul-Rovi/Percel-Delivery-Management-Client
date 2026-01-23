e55b5608f0491b403b89fdf23a82fb11aa62bd6f
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-23 23:49:45 +0600

    CLient Section Updated

--- src/Pages/Authentication/Register/Register.jsx
+++ src/Pages/Authentication/Register/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../../Hooks/UseAuth';
+import { Link } from 'react-router';
+import SocilaLogin from '../SocialLogin/SocilaLogin';
 
 const Register = () => {
     const {register , handleSubmit, formState:{errors}} = useForm();
@@ -19,9 +21,10 @@ const Register = () => {
     
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
+        <h1 className="text-5xl font-bold">Create An Account!</h1>
        <form onSubmit={handleSubmit(onSubmit)}>
          <fieldset className="fieldset">
-         <h1 className="text-5xl font-bold">Create An Account!</h1>
+       
 
           <label className="label">Email</label>
           <input type="email" {...register('email', {required: true})} className="input" placeholder="Email" />
@@ -43,9 +46,11 @@ const Register = () => {
 
           <div><a className="link link-hover">Forgot password?</a></div>
 
-          <button className="btn btn-neutral mt-4">Register</button>
+          <button className="btn btn-primary mt-4">Register</button>
         </fieldset>
+        <p><small>Already have an account? <Link className='btn-btn-link' to="/login">Login</Link> </small></p>
        </form>
+       <SocilaLogin></SocilaLogin>
       </div>
     </div>
 
